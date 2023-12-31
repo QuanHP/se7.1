97fce34d39a92313de0fa023b376bea8719240b9
nhq04072022 <148782229+nhq04072022@users.noreply.github.com>
2023-12-31 17:01:01 +0700

    Add files via upload
    fix bug find opponent

---
+++ MessageFindOpponent.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+
+public class MessageFindOpponent : MonoBehaviour
+{   
+    public Image myAvatar;
+    public Image enemyAvatar;
+    public GameObject findOpponentsBox;
+    [HideInInspector]
+    private bool isClose = false;
+    public void CallStart()
+    {
+        PlayerPrefs.DeleteKey("PUNCloudBestRegion");
+
+        Modules.fbEnemyAvatar = null;
+        myAvatar.sprite = Modules.fbMyAvatar;
+        if (myAvatar.sprite == null) myAvatar.sprite = Modules.iconAvatarNull;
+        
+        isClose = false;
+        
+    }
+
+    public void ButtonCloseBox()
+    {
+        findOpponentsBox.GetComponent<Animator>().SetTrigger("TriClose");
+        Modules.PlayAudioClipFree(Modules.audioButton);
+        transform.gameObject.SetActive(false);
+        Camera.main.GetComponent<PageMainGame>().ResetGame();
+
+        isClose = true;
+        // Gọi một phương thức hoặc thực hiện hành động khác để đóng hộp thoại.
+        
+    }
+}
